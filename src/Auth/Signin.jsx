a25e826506d029bea4687f85f04e831b615bc5c2
muhammedhanan9000 <mhdhananiqbal@gmail.com>
2025-11-26 11:47:57 +0530

    m

--- src/Auth/Signin.jsx
+++ src/Auth/Signin.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Signin = () => {
   return (
-    <div className='bg-rose-500'>Signin</div>
+    <div className='main w-full '>
+        <div className='flex justify-center items-center w-full h-screen'>
+            <div className="flex justify-center items-center bg bg-red-100 rounded-xl">
+                <div>
+                    hh
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
